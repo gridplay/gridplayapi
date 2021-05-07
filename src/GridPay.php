2e939cf9221fac15ea4871cf75eaaff832b007c3
Chrisx84 <chrisx84@live.ca>
2021-05-06 21:37:10 -0400

    Update GridPay.php

--- src/GridPay.php
+++ src/GridPay.php
@@ -1,28 +1,12 @@
 <?php
 namespace GridPlayAPI;
-use Illuminate\Support\Traits\Macroable;
 class GridPay {
-	use Macroable, Componentable {
-        Macroable::__call as macroCall;
-        Componentable::__call as componentCall;
-    }
 	public function __construct() {
 		//
 	}
-	public function getBal($uuid) {
+	public static function getBal($uuid) {
 		$a = ['type' => 'GET', 'url' => 'gridpay/getbal?uuid='.$uuid, 'body' => ''];
 		$api = GridPlayAPI::senddata($a);
 		return $api;
 	}
-	public function __call($method, $parameters) {
-        if (static::hasComponent($method)) {
-            return $this->componentCall($method, $parameters);
-        }
-
-        if (static::hasMacro($method)) {
-            return $this->macroCall($method, $parameters);
-        }
-
-        throw new BadMethodCallException("Method {$method} does not exist.");
-    }
 }
