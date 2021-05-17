2c52301bbbaa3e14c4ce3ac13092aad570262e63
Chrisx84 <chrisx84@live.ca>
2021-05-17 15:20:30 -0400

    integer fix

--- src/GridPay.php
+++ src/GridPay.php
@@ -3,17 +3,15 @@ namespace GridPlayAPI;
 use GridPlayAPI;
 class GridPay extends GridPlayAPI {
 	public static function getBal($uuid) {
-		$a = ['type' => 'GET',
-		'url' => 'gridpay/getbal?uuid='.$uuid];
+		$a = ['type' => 'GET','url' => 'gridpay/getbal?uuid='.$uuid];
 		$api = parent::senddata($a);
 		return $api;
 	}
 	public static function transfer($uuid, $payto, $amount) {
 		$j = ['uuid' => $uuid, 'payto' => $payto];
-		$j['amount'] = $amount;
+		$j['amount'] = str_replace(",","",$amount);
 		$j['gpn_key'] = config('gridplay.api_key');
-		$a = ['type' => 'POST',
-		'url' => 'gridpay/xfer', 'json' => $j];
+		$a = ['type' => 'POST','url' => 'gridpay/xfer', 'json' => $j];
 		$api = parent::senddata($a);
 		return $api;
 	}
