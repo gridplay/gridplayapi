2f108aae6c2c6bc7d5e4db3212ab56d7a5a8dd10
Ven Hansome <42349180+Chrisx84@users.noreply.github.com>
2022-06-29 20:13:22 -0400

    Update gridplay.php
    just removing the ad config since we dropped that service

--- config/gridplay.php
+++ config/gridplay.php
@@ -3,7 +3,4 @@ return [
 	// get this from https://gridplay.net/acc/settings
 	// mostly for POST method data
 	'api_key' => '',
-
-	// Ad rating, PG, MATURE, ADULT are only accepted
-	'ad_rating' => 'PG'
 ];
