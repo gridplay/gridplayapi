c5c7b3e99129b1713b6da007f184d2a62d63cf6b
Ven Hansome <42349180+Chrisx84@users.noreply.github.com>
2023-08-29 19:09:16 -0400

    Update GridPlay.php

--- src/GridPlay.php
+++ src/GridPlay.php
@@ -1,6 +1,7 @@
 <?php
 namespace GridPlayAPI;
 use GridPlayAPI;
+use Log;
 class GridPlay extends GridPlayAPI {
 	public static function Name2Key($uuid = "") {
 		$api = GridPlayAPI::senddata('GET','name2key/'.$uuid,[]);
@@ -22,6 +23,7 @@ class GridPlay extends GridPlayAPI {
 	}
 	public static function getSimCoords($sim) {
 		$api = GridPlayAPI::senddata('GET','coords/'.$sim,[]);
+		Log::debug($api);
 		return $api['coords'];
 	}
 	public static function getImg($uuid) {
