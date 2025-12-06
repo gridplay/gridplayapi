ac46708f195fb944a18331301fb0c47df76d7a2a
Venx84 <chrisx84@live.ca>
2025-12-06 16:31:28 -0500

    4.0.1

--- src/Ventalkie.php
+++ src/Ventalkie.php
@@ -5,4 +5,16 @@ class Ventalkie extends GridPlayAPI {
 		$api = parent::senddata('get', 'ventalkie', $search,[]);
 		return $api;
 	}
+	public static function sendMsg($chan = 1, $msg = "", $nick = null) {
+		$data = [];
+		if (is_null($nick) || $nick == "") {
+			$nick = env('APP_NAME');
+		}
+		$data['chan'] = $chan;
+		$data['nick'] = $nick;
+		$data['msg'] = $msg;
+		$data['owner'] = config('gridplay.slid');
+		$api = parent::senddata('put', 'ventalkie', 'send',$data);
+		return $api;
+	}
 }
